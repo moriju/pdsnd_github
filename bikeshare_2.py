99ea498d750746c3a3602817e4e5f9b5e270fdf4
Yulia <yulia.morina@olx.pl>
2023-01-29 12:32:56 +0100

    refactor: Changed wording for day input

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -27,7 +27,7 @@ def get_filters():
         month = input("Please choose ALL, January, February, March, April, May, June: ").lower()
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    day = input("Which day? Please choose: All, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday: ").lower()
+    day = input("Please choose a day: All, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday: ").lower()
     while day not in ["all","monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]:
         day = input("Please choose: All, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday: ").lower()
 
